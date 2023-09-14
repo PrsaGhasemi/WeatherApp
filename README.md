bc8a5f07877dedbf97e01cf6b16b4d8d4b86dcb2
Parsa Ghasemi <parsa.ghasemi2012@gmail.com>
2023-09-14 17:01:18 +0330

    adjusting parts

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # WeatherApp
  It's a weather app created with node.js and express.js using REST
+
+
+How does it work? 
+
+this project has one entry: the city name. then it goes through the first function that works to find lat,lon of the entered location using openWeather API, then it returns those info into the second API that finds the weather info using lat,lon. I created this connection with the third API 
--- app.js
+++ app.js
@@ -1,19 +1,16 @@
 const express = require('express');
-require('dotenv').config()
+require('dotenv').config("./src/.env")
 const app = express()
 
+const routes = require('./src/Routes/weatherRoutes');
+//* MiddleWares
 app.use(express.json())
 
 
-app.get("/salam" , (req,res) => {
-    console.log("kose nanashon");
-    res.send(200)
-})
+app.use("/weather" , routes)
 
-app.get("/weather" , (req,res) => {
 
-})
-
-app.listen(3000, () => {
-    console.log(`You are connected to port 3000`);
+PORT = process.env.PORT
+app.listen(PORT || 5000, () => {
+    console.log(`You are connected to port ${PORT}`);
 })
